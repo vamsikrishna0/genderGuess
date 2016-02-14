04884ea4603a0d5314c1da75fcc1dd9e7436b5a3
jetsasank <jet.sasank@gmail.com>
2016-02-13 19:40:40 -0500

    gmlmlmlm

--- src/main/java/com/cloud/genderguesser/controller/GenderGuesserController.java
+++ src/main/java/com/cloud/genderguesser/controller/GenderGuesserController.java
@@ -1,12 +1,17 @@
 package com.cloud.genderguesser.controller;
 
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
+import com.cloud.genderguesser.domain.GenderResponse;
 import com.cloud.genderguesser.service.GenderGuesserService;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 import io.swagger.annotations.ApiOperation;
@@ -21,7 +26,7 @@ public class GenderGuesserController {
 
 	   @ApiOperation(value = "getgender", nickname = "getgender")
 	   @RequestMapping(value = "/getgender", method = RequestMethod.GET)
-	   String getGender(@RequestParam String name) throws UnirestException  {
+	   GenderResponse getGender(@RequestParam String name) throws UnirestException, JsonParseException, JsonMappingException, IOException  {
 		   GenderGuesserService service = new GenderGuesserService();
 	       return service.getResponse(name);
 	   }
--- src/main/java/com/cloud/genderguesser/service/GenderGuesserService.java
+++ src/main/java/com/cloud/genderguesser/service/GenderGuesserService.java
@@ -1,18 +1,30 @@
 package com.cloud.genderguesser.service;
 
+import java.io.IOException;
+
+import org.json.JSONObject;
+
 import com.cloud.genderguesser.domain.GenderResponse;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.*;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class GenderGuesserService {
 
-	public String getResponse(String name) throws UnirestException {
+	public GenderResponse getResponse(String name) throws UnirestException, JsonParseException, JsonMappingException, IOException {
 
 HttpResponse<JsonNode> response = Unirest.get("https://montanaflynn-gender-guesser.p.mashape.com/?name=" + name)
 .header("X-Mashape-Key","SwnNz9MGm0mshWJ0ioMJ5t9bIIXmp1JVPufjsnkuMZriDiXucy")
 .header("Accept","application/json").asJson(); 
 System.out.print(response.getBody().toString());
-		return response.getBody().toString();
+JSONObject feedObject = new JSONObject(response.getBody().toString());
+ObjectMapper objectMapper = new ObjectMapper();
+objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+return objectMapper.readValue(feedObject.toString(), GenderResponse.class);
 		
 	}
 }
